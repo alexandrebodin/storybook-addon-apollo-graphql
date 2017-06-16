839c7c26dc794e8b854e1764474beb6387158481
Alexandre BODIN <bodin.alex@gmail.com>
2017-06-16 23:53:25 +0200

    Add bigger examples with shared root between stories

--- example/stories/index.js
+++ example/stories/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import randomStories from './random';
 import messageStories from './message';
 import mutationStories from './mutation';
+import variablesStories from './variables';
 
 randomStories();
 messageStories();
 mutationStories();
+variablesStories();
---
+++ example/stories/variables.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import Message from './components/Message';
+import { storiesOf } from '@storybook/react';
+import { withApolloProvider } from '../../src/index';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
+import { gql, graphql } from 'react-apollo';
+
+// define your schema
+const schema = `
+  type Message {
+    id: Int
+    content: String
+    author: String
+  }
+
+  input MessageInput {
+      content: String
+      author: String
+  }
+
+  type Query {
+    messages: [Message]
+  }
+
+  type Mutation {
+      updateMessage(id: ID!, input: MessageInput): Message
+  }
+`;
+
+// fake DB
+const messages = {
+  1: {
+    id: 1,
+    content: 'My StoryBook Content test',
+    author: 'My author content',
+  },
+};
+
+// a fake root with getMessages and update/create Messsage
+const root = {
+  messages: () => Object.values(messages),
+  updateMessage: ({ id, input }) => {
+    messages[id] = Object.assign(messages[id] || { id }, input);
+    return messages[id];
+  },
+};
+
+// A component to get the messages
+
+const query = gql`
+query {
+    messages {
+        id,
+        content,
+        author
+    }
+}
+`;
+
+const List = graphql(query)(({ data: { messages = [] } }) => {
+  return (
+    <div>
+      {messages.map((message, index) => {
+        return (
+          <ul key={index}>
+            <li>ID: {message.id}</li>
+            <li>Content: {message.content}</li>
+            <li>Author: {message.author}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+});
+
+// a component to udpate or create a message
+
+const mutation = gql`
+mutation ($id: ID! $author: String $content: String) {
+  updateMessage(id: $id input: {author: $author content: $content}) {
+    id
+  }
+}
+`;
+
+const Component = ({ mutate, id }) => {
+  return <button onClick={mutate}>Click to mutate message n°{id}</button>;
+};
+const ComponentWithMutation = graphql(mutation, {
+  options: ({ id, content, author }) => ({ variables: { id, content, author } }),
+})(Component);
+
+/**
+ * You can navigate between the two stories to see how you can add and update the same DB 
+ * and play arround with your components
+ */
+
+export default () => {
+  storiesOf('Variables', module)
+    .addDecorator(withApolloProvider({ schema, root }))
+    .addDecorator(withKnobs)
+    .add('List of messages', () => <List />)
+    .add('Mutation of messages', () => {
+      const id = number('ID', 1);
+      const content = text('Content', 'Hello');
+      const author = text('Author', 'I.Asimov');
+
+      return <ComponentWithMutation id={id} content={content} author={author} />;
+    });
+};
